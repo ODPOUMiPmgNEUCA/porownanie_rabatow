f6e341300c8d303fa0966113fbb03ec950e48672
ODPOUMiPmgNEUCA <mgroblica@neuca.pl>
2025-02-12 17:59:53 +0100

    Update porownanierabatow.py

--- porownanierabatow.py
+++ porownanierabatow.py
@@ -110,10 +110,10 @@ with pd.ExcelWriter(excel_file1, engine='xlsxwriter') as writer:
     worksheet = writer.sheets["dane"]
 
     # Definiowanie formatu liczbowego z przecinkiem dziesiętnym
-    format_percent = workbook.add_format({"num_format": "0,00"})  # Dwa miejsca po przecinku
+    #format_percent = workbook.add_format({"num_format": "0,00"})  # Dwa miejsca po przecinku
 
     # Ustalenie szerokości kolumn i zastosowanie formatu (dla całego zakresu kolumn)
-    worksheet.set_column("B:Z", None, format_percent)  # Dostosuj zakres kolumn do swojego pliku
+    #worksheet.set_column("B:Z", None, format_percent)  # Dostosuj zakres kolumn do swojego pliku
 
 # Resetowanie wskaźnika do początku pliku
 excel_file1.seek(0)
