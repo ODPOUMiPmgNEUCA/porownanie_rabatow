fc6508037dee1ef02d893cfabe66679bfb114ac1
ODPOUMiPmgNEUCA <mgroblica@neuca.pl>
2025-02-13 11:44:01 +0100

    Update porownanierabatow.py

--- porownanierabatow.py
+++ porownanierabatow.py
@@ -45,10 +45,9 @@ st.title("Automat PORÓWNANIE RABATÓW - Instrukcja obsługi")
 st.markdown("""
 ### Jak korzystać z aplikacji?
 1. Nie przejmuj się, że strona wyświetla błąd: **TypeError**. Będzie on widoczny, dopóki nie wrzucisz pliku z Raportem promocyjnym, który codziennie dostajesz na maila :)
-2. Jak go wrzucić?
+2. Jak go wrzucić?  
     2.1. Pobierz folder z maila.  
-    2.2.
-    2.2. Folder pobierze się w formacie **.zip**.
+    2.2. Folder pobierze się w formacie **.zip**.  
     2.3. 
 
 """)
