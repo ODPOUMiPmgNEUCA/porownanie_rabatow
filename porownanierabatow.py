6dfcad57715e39ee21e2b449ee8eca13bcb934e2
ODPOUMiPmgNEUCA <mgroblica@neuca.pl>
2025-02-13 12:03:40 +0100

    Update porownanierabatow.py

--- porownanierabatow.py
+++ porownanierabatow.py
@@ -53,7 +53,7 @@ st.markdown("""
 
 st.image("potrzebne_fotki/zip.png")
 
-st.markdown("""&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2.3. Rozpakuj pobrany folder .zip, klikając na niego prawym przyciskiem myszy i wybierając opcję **Wyodrębnij wszystkie**, unsafe_allow_html=True""")
+st.markdown("""&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2.3. Rozpakuj pobrany folder .zip, klikając na niego prawym przyciskiem myszy i wybierając opcję **Wyodrębnij wszystkie**""", unsafe_allow_html=True)
 
 
 
