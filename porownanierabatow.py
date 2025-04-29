7e32a5172cf91b0fae77aca7b12ee126fe89c3aa
ODPOUMiPmgNEUCA <mgroblica@neuca.pl>
2025-04-29 09:14:36 +0200

    Update porownanierabatow.py

--- porownanierabatow.py
+++ porownanierabatow.py
@@ -156,6 +156,8 @@ pivot_table
 pivot_table1 = pivot_table.reset_index()
 # Wybór tylko konkretnych kolumn (np. "Promocja A" i "Promocja B")
 selected_columns = ["Nazwa producenta sprzedażowego", "Id Materiału", "Nazwa Materiału", "IPRA", "EO", "ŚZ/P", "RPM", "ZGZ", "sieci", "centralne"]
+print("Kolumny w pivot_table1:", pivot_table1.columns.tolist())
+print("Wybrane kolumny:", selected_columns)
 pivot_table1 = pivot_table1[selected_columns]
 
 #pivot_table1
@@ -165,7 +167,7 @@ pivot_table1 = pivot_table1[selected_columns]
 selected2 = ["Nazwa producenta sprzedażowego", "Id Materiału", "Nazwa Materiału", "IPRA", "EO", "ŚZ/P"]
 pivot_table2 = pivot_table1[selected2]
 pivot_table2 = pivot_table2.dropna(subset=["IPRA", "EO", "ŚZ/P"], how="all")
-pivot_table2
+#pivot_table2
 
 
 # Są w IPRA, nie ma w ŚZ/P
